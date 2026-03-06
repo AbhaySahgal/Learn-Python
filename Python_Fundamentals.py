d60fd30b5aa2e3e3a421ad3962c2fb148b94c0c6
Abhay Sahgal <abhaysahgal1998@gmail.com>
2026-03-06 19:12:25 +0530

    Operators

--- Python_Fundamentals.py
+++ Python_Fundamentals.py
@@ -1,17 +1,17 @@
 #Basic printing in python
 
-print("Hello, World!")
-print("My name is Abhay")
-print("I am learning Python")
-print("This is my first program")
+# print("Hello, World!")
+# print("My name is Abhay")
+# print("I am learning Python")
+# print("This is my first program")
 
 #Variables in python
 
-name = "Abhay"
-age = 25
+# name = "Abhay"
+# age = 25
 
-print(name)
-print(age)
+# print(name)
+# print(age)
 
 #exercise
 
@@ -21,10 +21,37 @@ print(age)
 # I am learning Python
 # I will become a Python developer
 
-name = "Abhay"
-lang = "Python"
-role = "Python developer"
+# name = "Abhay"
+# lang = "Python"
+# role = "Python developer"
 
-print("My name is", name)
-print("I am learning", lang)
-print("I will become a", role)
+# print("My name is", name)
+# print("I am learning", lang)
+# print("I will become a", role)
+
+
+#Input in Python
+
+# name = input("Enter your Name:")
+# print("Hello,", name) #input always return string
+
+# age = input("Enter your Age:")
+# print(type(age))
+
+#For use as Int or other the string we can typeconversion
+
+# age = int(input("Enter your age:" ))
+# print(type(age))
+
+num1 = int(input("Enter first Number:" ))
+num2 = int(input("Enter second Number:" ))
+
+Addition = num1 + num2
+Substraction = num1 - num2
+Multiplication = num1 * num2
+Division = num1 / num2
+Modulas = num1 % num2
+Power = num1 ** num2
+Floor_Division = num1 // num2
+
+print(Addition , Substraction , Multiplication , Division, Modulas, Power, Floor_Division)
