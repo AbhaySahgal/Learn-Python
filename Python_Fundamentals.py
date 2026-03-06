10f8babf369221c739d80146380dc2ac9f40d7c8
Abhay Sahgal <abhaysahgal1998@gmail.com>
2026-03-06 18:39:21 +0530

    First Exercise

--- Python_Fundamentals.py
+++ Python_Fundamentals.py
@@ -1,4 +1,30 @@
+#Basic printing in python
+
 print("Hello, World!")
 print("My name is Abhay")
 print("I am learning Python")
 print("This is my first program")
+
+#Variables in python
+
+name = "Abhay"
+age = 25
+
+print(name)
+print(age)
+
+#exercise
+
+# Write a program that prints:
+
+# My name is Abhay
+# I am learning Python
+# I will become a Python developer
+
+name = "Abhay"
+lang = "Python"
+role = "Python developer"
+
+print("My name is", name)
+print("I am learning", lang)
+print("I will become a", role)
