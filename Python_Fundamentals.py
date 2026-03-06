ed38d24f62577a77a27a7cec6b234c2f98d8cc52
Abhay Sahgal <abhaysahgal1998@gmail.com>
2026-03-06 19:50:23 +0530

    Login System

--- Python_Fundamentals.py
+++ Python_Fundamentals.py
@@ -21,6 +21,10 @@
 # I am learning Python
 # I will become a Python developer
 
+
+
+
+
 # name = "Abhay"
 # lang = "Python"
 # role = "Python developer"
@@ -28,6 +32,9 @@
 # print("My name is", name)
 # print("I am learning", lang)
 # print("I will become a", role)
+
+
+
 
 
 #Input in Python
@@ -38,20 +45,82 @@
 # age = input("Enter your Age:")
 # print(type(age))
 
+
+
+
+
 #For use as Int or other the string we can typeconversion
 
 # age = int(input("Enter your age:" ))
 # print(type(age))
 
-num1 = int(input("Enter first Number:" ))
-num2 = int(input("Enter second Number:" ))
 
-Addition = num1 + num2
-Substraction = num1 - num2
-Multiplication = num1 * num2
-Division = num1 / num2
-Modulas = num1 % num2
-Power = num1 ** num2
-Floor_Division = num1 // num2
 
-print(Addition , Substraction , Multiplication , Division, Modulas, Power, Floor_Division)
+
+# num1 = int(input("Enter first Number:" ))
+# num2 = int(input("Enter second Number:" ))
+
+# Addition = num1 + num2
+# Substraction = num1 - num2
+# Multiplication = num1 * num2
+# Division = num1 / num2
+# Modulas = num1 % num2
+# Power = num1 ** num2
+# Floor_Division = num1 // num2
+
+# print(Addition , Substraction , Multiplication , Division, Modulas, Power, Floor_Division)
+
+
+
+
+
+# Check if number is odd or even
+
+# num = int(input("Enter number: "))
+
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+
+
+#Comparison Operator
+
+# | Operator | Meaning          | Example |
+# | -------- | ---------------- | ------- |
+# | ==       | equal            | x == y  |
+# | !=       | not equal        | x != y  |
+# | >        | greater than     | x > y   |
+# | <        | less than        | x < y   |
+# | >=       | greater or equal | x >= y  |
+# | <=       | less or equal    | x <= y  |
+
+#elif 
+
+# marks = int(input("Enter marks: "))
+
+# if marks >= 90:
+#     print("Grade A")
+
+# elif marks >= 75:
+#     print("Grade B")
+
+# elif marks>= 50:
+#     print("Grade C")
+
+# else:
+#     print("Fail")
+
+
+#Login System
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin" and password == "python123":
+    print("Login Successful")
+
+else:
+    print("Invalid credentials")
