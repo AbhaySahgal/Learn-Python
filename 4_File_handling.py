006b9db072bf3caf735decc905084d992cc80cb4
Abhay Sahgal <abhaysahgal1998@gmail.com>
2026-03-07 19:45:52 +0530

    File Handling

---
+++ 4_File_handling.py
@@ -0,0 +1,12 @@
+#File Handling
+
+#Syntax
+# file = open("filename", "mode")
+
+#Modes
+# | Mode | Meaning      |
+# | ---- | ------------ |
+# | r    | read         |
+# | w    | write        |
+# | a    | append       |
+# | r+   | read + write |
