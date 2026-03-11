f79f37c59c70edaff6a078887ad467b72b75e4f1
Abhay Sahgal <abhaysahgal1998@gmail.com>
2026-03-11 21:57:46 +0530

    Tuples Finished

--- 9_Tuples.py
+++ 9_Tuples.py
@@ -28,3 +28,52 @@ print(age)
 
 
 
+
+
+
+
+
+
+#Task 1
+
+numbers = (5,10,15,20)
+
+print(numbers)
+
+
+
+
+
+
+
+
+
+
+
+#Task 2
+person = ("Abhay", 25, "India")
+
+print(person[0])
+print(person[1])
+print(person[2])
+
+
+
+
+
+
+
+
+
+
+import math
+
+point1 = (2, 3)
+point2 = (5, 7)
+
+x1, y1 = point1
+x2, y2 = point2
+
+val = math.sqrt(((x2-x1)**2 + (y2-y1)**2))
+
+print("Distance btw", point1, "and", point2, "is: ", val)
